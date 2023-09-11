a9514152edb852a1728b5494be502e085b73f811
eTcilopp <kirikeza@gmail.com>
2023-09-11 08:05:26 -0600

    problem 4 done

---
+++ homework_3.sql
@@ -0,0 +1,40 @@
+-- Написать скрипт, возвращающий список имен (только firstname) пользователей без повторений в алфавитном порядке. [ORDER BY]
+
+select distinct 
+	us.firstname 
+from 
+	users us
+order by
+	us.firstname
+
+-- Выведите количество мужчин старше 35 лет [COUNT].
+
+select 
+	count(*)
+from 
+	profiles pr
+where 
+	pr.gender = 'm'
+	and timestampdiff(year, pr.birthday, curdate()) > 35
+
+-- Сколько заявок в друзья в каждом статусе? (таблица friend_requests) [GROUP BY]
+
+select
+	fr.status,
+	count(fr.status )
+from 
+	friend_requests fr 
+group by
+	fr.status 
+
+-- * Выведите номер пользователя, который отправил больше всех заявок в друзья (таблица friend_requests) [LIMIT].
+select 
+    fr.initiator_user_id as user_issued_max_friend_requests
+from 
+	friend_requests fr 
+group by
+	fr.initiator_user_id
+order by 
+	count(fr.initiator_user_id) desc
+limit 1
+	
